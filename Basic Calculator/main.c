b2ffe7d6315ad7725cd5b6d2603689a034f81cbf
Rujuta <rujutabudke@gmail.com>
2023-03-18 18:56:33 +0530

    Update

--- Basic Calculator/main.c
+++ Basic Calculator/main.c
@@ -1,38 +1,20 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include "header.h"
+
+typedef struct node{
+    int data;
+    struct node *next;
+} node;
+
+typedef struct stack{
+    node *data;
+    struct stack *next;
+} stack;
 
 void main(){
-	node *n1, *n2;
-	init(&n1);
-	init(&n2);
-	//insert(&n1, 1);
-	
-	char *A;
+	char A[100];
 	printf("bc\n");
-	A = "1234+5678";
-	printf("%s\n", A);
+	scanf("%s", A);
+	//printf("%s\n", A);
 	int s = strlen(A);
-	
-	//first number
-	int i = 0;
-	while(i < s && isdigit(A[i]) != 0) { //A[i] - '0' >= 0 && A[i] - '0' <= 9){
-		//printf("%c\n", A[i]);
-		insert(&n1, A[i] - '0');
-		i++;
-	}
-	//display(n1);
-
-	i += 1; //skip '+' symbol
-	
-	//second number
-	for(; i < s; i ++){
-		//printf("%c\n", A[i]);
-		insert(&n2, A[i] - '0');
-	}
-	//display(n2);
-	
-	display(add(n1, n2));
+	evaluate(A, s);
 }
